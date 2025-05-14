05dc58dd73e622c4e78e82d12394ff1e1fa60ec3
AnthonGallo <anthongallo2003@gmail.com>
2025-05-13 21:41:30 -0500

    Inicio.

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new Views.vPagos());
+            return new Window(new Views.vLogIn());
         }
     }
 }
---
+++ Views/vInicio.xaml.cs
@@ -0,0 +1,9 @@
+namespace KynosPetClub.Views;
+
+public partial class vInicio : ContentPage
+{
+	public vInicio()
+	{
+		InitializeComponent();
+	}
+}
