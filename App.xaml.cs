196fc1281effbd8071c50aab2f8f5ad2358dec9d
tony <e1720429891@uisrael.edu.ec>
2025-05-13 21:33:27 -0500

    Ventala de Pagos

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new Views.vReserva());
+            return new Window(new Views.vLogIn());
         }
     }
 }
