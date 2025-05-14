d7930a2d3cba514475d2254282706860ae9b3dd6
master <master@SMLAB306PC05_25>
2025-05-13 21:47:06 -0500

    Planes

---
+++ Views/vPlanes.xaml.cs
@@ -0,0 +1,19 @@
+namespace KynosPetClub.Views;
+
+public partial class vPlanes : ContentPage
+{
+	public vPlanes()
+	{
+		InitializeComponent();
+	}
+
+    private void btnAñadir1_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnAñadir2_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
